06a37080675d596241af33e997af041e8f0d38ea
Sumitsskai1 <masaki_sekiguchi@smtss.jp>
2023-03-23 18:12:31 +0900

    Initial Commit

---
+++ Demo2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo2
+//
+//  Created by SuMiTS&S 開発第一部 on 2023/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
